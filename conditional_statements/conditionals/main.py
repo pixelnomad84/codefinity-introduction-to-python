1064433942266e6b6f3ac1ecc306e9abdff724c1
pixelnomad84 <>
2026-03-01 15:47:51 +0000

    Conditionals

--- conditional_statements/conditionals/main.py
+++ conditional_statements/conditionals/main.py
@@ -1,9 +1,14 @@
 # Input variables - Discount Strategy System
-product_type = "Dairy"  
-day_of_week = "Wednesday"
+product_type = "Vegetables"  
+day_of_week = "Tuesday"
 if product_type == "Fruits" and day_of_week == "Monday":
     print ("10% discount on Fruits today!")
 elif product_type == "Vegetables" and day_of_week == "Tuesday":
     print ("15% discount on Vegetables today!")
-    
+elif product_type == "Dairy" and day_of_week == "Wednesday":
+    print ("20% discount on Dairy Today")
+elif product_type == "Other":
+    print ("No discount available")
+else:
+    print ("No special discounts today")
     
